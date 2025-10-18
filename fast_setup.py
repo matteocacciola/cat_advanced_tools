1c30b9fbf368f07ba05908d4951ff6d5800be5a4
matteocacciola <matteo.cacciola@gmail.com>
2025-10-18 20:24:25 +0200

    Rename and simplify memory recall functions
    Refactor memory recall functions and clean up unused code.

--- fast_setup.py
+++ fast_setup.py
@@ -11,16 +11,7 @@ def agent_prompt_prefix(prefix, cat):
 
 
 @hook
-def before_cat_recalls_episodic_memories(default_episodic_recall_config, cat):
-    settings = cat.mad_hatter.get_plugin().load_settings()
-    default_episodic_recall_config["k"] = settings["episodic_memory_k"]
-    default_episodic_recall_config["threshold"] = settings["episodic_memory_threshold"]
-
-    return default_episodic_recall_config
-
-
-@hook
-def before_cat_recalls_declarative_memories(default_declarative_recall_config, cat):
+def before_cat_recalls_memories(default_declarative_recall_config, cat):
     settings = cat.mad_hatter.get_plugin().load_settings()
     default_declarative_recall_config["k"] = settings["declarative_memory_k"]
     default_declarative_recall_config["threshold"] = settings[
@@ -31,39 +22,12 @@ def before_cat_recalls_declarative_memories(default_declarative_recall_config, c
 
 
 @hook
-def before_cat_recalls_procedural_memories(default_procedural_recall_config, cat):
-    settings = cat.mad_hatter.get_plugin().load_settings()
-    default_procedural_recall_config["k"] = settings["procedural_memory_k"]
-    default_procedural_recall_config["threshold"] = settings[
-        "procedural_memory_threshold"
-    ]
-
-    return default_procedural_recall_config
-
-
-#@hook
-#def before_agent_starts(agent_input, cat):
-#    settings = cat.mad_hatter.get_plugin().load_settings()
-#    user_name = settings["user_name"]
-#    agent_input["chat_history"] = agent_input["chat_history"].replace(
-#        "- Human:", f"- {user_name}:"
-#    )
-#
-#    return agent_input
-
-
-@hook
 def agent_prompt_suffix(suffix, cat):
     settings = cat.mad_hatter.get_plugin().load_settings()
     username = settings["user_name"] if settings["user_name"] != "" else "Human"
     suffix = f"""
 # Context
-
-{{episodic_memory}}
-
-{{declarative_memory}}
-
-{{tools_output}}
+{{context}}
 """
 
     if settings["language"] == "Human":
@@ -74,9 +38,6 @@ ALWAYS answer in the {username}'s language
         suffix += f"""
 ALWAYS answer in {settings["language"]}
 """
-
-    suffix += f"""
-## Conversation until now:"""
 
     return suffix
 
